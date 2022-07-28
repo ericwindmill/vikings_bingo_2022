a6ccd21d29878bae31c93d6b54d149931eb24fe0
Eric Windmill <eric@ericwindmill.com>
2022-07-28 13:21:02 -0700

    Change firebase project (#2)
    * checkin seeds
    
    * change firebase project;
    
    * update actions
    
    Co-authored-by: Eric Windmill <ewindmill@google.com>

--- player_app/lib/main.dart
+++ player_app/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared/player.dart';
@@ -20,14 +19,14 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  if (!kReleaseMode) {
-    try {
-      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
-      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-    } catch (e) {
-      debugPrint(e.toString());
-    }
-  }
+  // if (!kReleaseMode) {
+  //   try {
+  //     FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  //     await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  //   } catch (e) {
+  //     debugPrint(e.toString());
+  //   }
+  // }
 
   Player player = await _bootstrapPlayer();
 
