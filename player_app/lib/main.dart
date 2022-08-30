8a906c495b677acfdd128b6d084d7b4a5e8f7b7c
Eric Windmill <ewindmill@google.com>
2022-08-30 10:55:05 +0200

    remove un-used code

--- player_app/lib/main.dart
+++ player_app/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shared/player.dart';
 
 import 'firebase_options.dart';
 import 'src/app_refactor.dart';
-import 'src/util/game_util.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,17 +14,4 @@ void main() async {
   await FirebaseAuth.instance.signInAnonymously();
 
   runApp(AppShell());
-}
-
-Future<Player> bootstrapPlayer() async {
-  FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-    if (user == null) return;
-
-    if (user.displayName == null) {
-      await user.updateDisplayName(generateRandomPlayerName());
-    }
-  });
-
-  final cred = await FirebaseAuth.instance.signInAnonymously();
-  return Player(uid: cred.user!.uid, name: cred.user!.displayName!);
 }
